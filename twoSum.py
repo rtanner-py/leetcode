6c5086a70443e6e90b00ba93926068971118cd16
rtanner-py <rob.tanner@gmail.com>
2025-03-03 20:59:35 +0000

    Dictionary solution
    Goes from O(n^2) time complexity to O(n)

--- twoSum.py
+++ twoSum.py
@@ -12,8 +12,19 @@ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
 
 class Solution:
+    """
+    #this is a poor solution as it has O(n^2) time complexity and will be terrible for larger sets.
+    #need to find a better way. Likely some form of hashed solution that keeps track of what we have already seen.
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(0,len(nums)):
             for j in range(i+1, len(nums)):
                 if (nums[i]+nums[j]) == target:
                     return [i,j]
+    """
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            if target - num in num_dict:
+                return [num_dict[target - num], i]
+            num_dict[num] = i
+        return
